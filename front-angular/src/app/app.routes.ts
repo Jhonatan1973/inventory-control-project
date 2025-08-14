ad19c78c01d4ab398c4594ada2cc13f4521f5ccd
Rodrigo <rodrigobaesa@gmail.com>
2025-08-14 16:27:49 -0300

    [front] feat: add forgot-password component

--- front-angular/src/app/app.routes.ts
+++ front-angular/src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { AuthGuard } from './guard/AuthGuard';
 import { StockComponent } from './pages/stock/stock.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'stock', component: StockComponent, canActivate: [AuthGuard]},
   { path: 'register', component: RegisterComponent, canActivate: [AuthGuard]},
