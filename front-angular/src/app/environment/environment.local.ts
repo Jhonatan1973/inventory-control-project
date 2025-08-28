2bbe32c83c49d8076e9bd6965454db93a8aa428c
Jhonatan <Jhonatandomingues1122@outlook.com>
2025-08-28 10:58:02 -0300

    fix: adjusting IP API

--- front-angular/src/app/environment/environment.local.ts
+++ front-angular/src/app/environment/environment.local.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:9090/api',
+  apiUrl: 'http://10.1.0.45:9090/api',
   endpoints: {
     auth: '/auth/login',
     users: '/users',
